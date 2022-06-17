6750e0d02384d6154e38a4b6b8f1a032b96e0c29
Jono453 <32658975+Jono453@users.noreply.github.com>
2022-06-17 14:32:50 +1000

    add TCA9554 test code

--- libraries/AP_ICEngine/AP_ICEngine.h
+++ libraries/AP_ICEngine/AP_ICEngine.h
@@ -18,12 +18,14 @@
  */
 #pragma once
 
-#include <AP_Param/AP_Param.h>
+#include <AP_HAL/AP_HAL.h>
+#include <AP_RPM/AP_RPM.h>
+#include <AP_HAL/I2CDevice.h>
 
 class AP_ICEngine {
 public:
     // constructor
-    AP_ICEngine(const class AP_RPM &_rpm);
+    AP_ICEngine(const AP_RPM &_rpm);
 
     static const struct AP_Param::GroupInfo var_info[];
 
@@ -31,7 +33,7 @@ public:
     void update(void);
 
     // check for throttle override
-    bool throttle_override(float &percent);
+    bool throttle_override(uint8_t &percent);
 
     enum ICE_State {
         ICE_OFF=0,
@@ -53,15 +55,44 @@ public:
     static AP_ICEngine *get_singleton() { return _singleton; }
 
 private:
+
+ /*
+     * TCA9554 output register mapping for PMB Rev E
+     * P0 = PMU_EN - PMU output ON/OFF  (CN6 pin 2)
+     * P1 = ECU_EN - ECU Kill switch (CN5 pin 3)
+     * P2 = I2C_P2 - Unused
+     * P3 = LED (active low)
+     * P4 = PMU_START - Crank Direction (CN6 pin 5)
+     * P5 = PMU_ARM  - Crank Signal (CN6 pin 6)
+     * P6 = PMU_STAT_IN - Unused
+     * P7 = PMU_STAT - Unused
+     */
+
+    enum TCA9554_state_t {
+        IGN_OFF_STR_OFF = 0x30,	 		//output register - 0011 0000
+        IGN_ON_STR_OFF = 0x33,		 	//output register - 0011 0011
+		IGN_ON_STR_ON_DIR_ON = 0x03, 	//output register - 0000 0011
+    } TCA9554_state = IGN_OFF_STR_OFF;
+
+    enum i2c_init_t {
+        I2C_UNINITIALIZED = 0,
+        I2C_FAILED = 1,
+        I2C_SUCCESS = 2
+    } i2c_state = I2C_UNINITIALIZED;
+
+    bool TCA9554_init();
+
+    void TCA9554_set(TCA9554_state_t value);
+
+    void control_ign_str(TCA9554_state_t value);
+
+    AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev_TCA9554;
+
     static AP_ICEngine *_singleton;
 
-    const class AP_RPM &rpm;
+    const AP_RPM &rpm;
 
     enum ICE_State state;
-
-    // filter for RPM value
-    LowPassFilterFloat _rpm_filter;
-    float filtered_rpm_value;
 
     // enable library
     AP_Int8 enable;
@@ -125,21 +156,12 @@ private:
 
     enum class Options : uint16_t {
         DISABLE_IGNITION_RC_FAILSAFE=(1U<<0),
-        DISABLE_REDLINE_GOVERNOR = (1U << 1),
     };
     AP_Int16 options;
 
     // start_chan debounce
     uint16_t start_chan_last_value = 1500;
     uint32_t start_chan_last_ms;
-
-    // redline rpm
-    AP_Int32 redline_rpm;
-    struct {
-        bool flag;
-        float governor_integrator;
-        float throttle_percentage;
-    } redline;
 };
 
 
